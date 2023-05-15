dd07eb0b796afd5d251c9b9246f92fcffadcafc2
cfyoc <99231469+cfyoc@users.noreply.github.com>
2023-05-15 11:32:58 -0400

    Initial Commit

---
+++ TownieSwift/TownieSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TownieSwiftApp.swift
+//  TownieSwift
+//
+//  Created by Colin Fyock on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TownieSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
